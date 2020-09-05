20e7f41aa5d82cb900c39627053a77cd58f2fa2e
hieuvu99 <star.hieu.99@gmail.com>
2020-09-05 22:02:13 +0700

    get location permission from user. SDK 23 and later

--- Disneyworld/app/src/main/java/com/hieuwu/disneyworld/MapsActivity.kt
+++ Disneyworld/app/src/main/java/com/hieuwu/disneyworld/MapsActivity.kt
@@ -1,12 +1,23 @@
 package com.hieuwu.disneyworld
 
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.location.Location
+import android.location.LocationListener
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptor
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
@@ -21,6 +32,40 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        checkPermission()
+    }
+
+    var ACCESSLOCATION = 123
+    fun checkPermission() {
+        if(Build.VERSION.SDK_INT >= 23) {
+            if(ActivityCompat.checkSelfPermission(this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), ACCESSLOCATION)
+                return
+            }
+        }
+        GetUserLocation()
+    }
+
+    fun GetUserLocation() {
+        Toast.makeText(this,"Location access allowed !", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode) {
+            ACCESSLOCATION -> {
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    GetUserLocation()
+                } else {
+                    Toast.makeText(this,"Location access now allowed !", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
 
     /**
@@ -35,9 +80,43 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
+        var profileMarker = resizeMarkerIcon(R.drawable.insect,128,128)
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val saigon = LatLng(10.779, 106.699)
+        mMap.addMarker(MarkerOptions().position(saigon).title("Me").snippet("Here is my location").icon(profileMarker))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(saigon,14f))
+
+    }
+
+    private fun resizeMarkerIcon(id:Int, width: Int, height: Int): BitmapDescriptor {
+        val bitmapDrawable = BitmapFactory.decodeResource(resources,id)
+        val resizedBitmap = Bitmap.createScaledBitmap(bitmapDrawable,width,height,false)
+        var iconInBitmap = BitmapDescriptorFactory.fromBitmap(resizedBitmap);
+        return iconInBitmap
+    }
+
+    inner class MyLocationListener: LocationListener {
+        var location:Location?=null
+        constructor() {
+            location = Location("Start")
+            location!!.longitude = 0.0
+            location!!.latitude = 0.0
+        }
+        override fun onLocationChanged(p0: Location?) {
+            this.location = p0
+    }
+
+        override fun onStatusChanged(p0: String?, p1: Int, p2: Bundle?) {
+           // TODO("Not yet implemented")
+        }
+
+        override fun onProviderEnabled(p0: String?) {
+          //  TODO("Not yet implemented")
+        }
+
+        override fun onProviderDisabled(p0: String?) {
+           // TODO("Not yet implemented")
+        }
+
     }
 }
