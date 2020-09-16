403f2a63fc3f11cfe957a54859a354df3776acb6
hieuvu99 <star.hieu.99@gmail.com>
2020-09-16 21:41:23 +0700

    add interface model for ToDo

--- MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/model/bean/db/ToDoModel.kt
+++ MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/model/bean/db/ToDoModel.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.mvcdemo.model.bean.db
 
+import com.hieuwu.mvcdemo.model.bean.ToDo
+import java.util.*
+import kotlin.collections.ArrayList
+
 interface ToDoModel {
+    fun getAllToDoList(): ArrayList<ToDo>
+    fun  addToDoItem(item: ToDo): Boolean
+    fun removeToDoItem(id: Int): Boolean
+    fun editToDoItem(atitle:String, aContent: String, modifedAt: Date): Boolean
 }
