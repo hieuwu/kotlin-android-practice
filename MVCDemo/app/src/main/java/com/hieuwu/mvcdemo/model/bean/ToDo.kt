e5355c215140fc36838342506928da78496b6dd0
hieuvu99 <star.hieu.99@gmail.com>
2020-09-16 21:41:02 +0700

    add bean kotlin class of todo object

--- MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/model/bean/ToDo.kt
+++ MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/model/bean/ToDo.kt
@@ -1,4 +1,33 @@
-package com.hieuwu.mvcdemo.model
+package com.hieuwu.mvcdemo.model.bean
+
+import java.lang.IllegalArgumentException
+import java.util.*
 
 class ToDo {
+    var title:String?
+        set(value) {
+            this.title = value
+        }
+        get() {
+            return this.title
+        }
+    var content:String?
+        set(value) {
+            this.content = value
+        }
+         get() {
+             return this.content
+         }
+
+    var createdAt:Date? = null
+        set(value) {
+            this.createdAt = value
+            field = value
+        }
+
+    constructor(aTitle: String, aContent: String, createdAt: Date) {
+        this.title = aTitle
+        this.content = aContent
+        this.createdAt = createdAt
+    }
 }
