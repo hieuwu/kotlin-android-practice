389e0e782a33c88e8d0d877e1ec4c566f31b1196
hieuvu99 <star.hieu.99@gmail.com>
2020-09-16 21:42:29 +0700

    add view layer

--- MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/view/ToDoMainActivityView.kt
+++ MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/view/ToDoMainActivityView.kt
@@ -1,4 +1,12 @@
 package com.hieuwu.mvcdemo.view
 
-interface ToDoMainActivityView {
+import com.hieuwu.mvcdemo.model.bean.ToDo
+
+interface ToDoMainActivityView: ToDoView {
+    fun bindDataToView()
+    fun showAllToDos(todoList: ArrayList<ToDo>)
+    fun updateViewOnAdd(todoList: ArrayList<ToDo>)
+    fun updateViewOnRemove(todoList: ArrayList<ToDo>)
+    fun updateViewOnEdit(todoList: ArrayList<ToDo>)
+    fun  showErrorToast(errorMessage: String)
 }
