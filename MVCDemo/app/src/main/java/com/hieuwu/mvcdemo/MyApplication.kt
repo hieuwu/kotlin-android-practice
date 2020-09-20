80eb0a846b4a0684f2bdb75da9487b6dbbe92da1
hieuvu99 <star.hieu.99@gmail.com>
2020-09-20 14:18:17 +0700

    add kotlin class for creating adapter

--- MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/MyApplication.kt
+++ MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/MyApplication.kt
@@ -1,4 +1,18 @@
 package com.hieuwu.mvcdemo
 
-class MyApplication {
+import android.app.Application
+import com.hieuwu.mvcdemo.model.bean.ToDoListAdapter
+class MyApplication: Application() {
+    companion object {
+        private var toDoListAdapter: ToDoListAdapter ?= null
+        fun  getToDoListAdapter(): ToDoListAdapter? {
+            return toDoListAdapter
+        }
+
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        toDoListAdapter = ToDoListAdapter.getToDoListAdapterInstance(this)
+    }
 }
