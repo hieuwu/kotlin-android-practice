44cf71cdc424ea7492063a8d185cb9cf47e853e3
hieuvu99 <star.hieu.99@gmail.com>
2020-09-24 13:19:28 +0700

    refactor todo controller

--- MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/controller/ToDoController.kt
+++ MVCDemo/app/src/main/java/com/hieuwu/mvcdemo/controller/ToDoController.kt
@@ -1,20 +1,21 @@
 package com.hieuwu.mvcdemo.controller
-
 import com.hieuwu.mvcdemo.MainActivity
 import com.hieuwu.mvcdemo.model.bean.ToDo
 import com.hieuwu.mvcdemo.model.bean.db.ToDoModelImplementor
+import com.hieuwu.mvcdemo.view.MainActivityViewImplementor
+
 import java.lang.Exception
 
 class ToDoController {
     var todoModel: ToDoModelImplementor? = null
-    var todoView: MainActivityImplementor? = null
-    constructor(todoModel :ToDoModelImplementor, todoView: MainActivityImplementor) {
+    var todoView: MainActivityViewImplementor? = null
+    constructor(todoModel :ToDoModelImplementor, todoView: MainActivityViewImplementor) {
         this.todoModel = todoModel
         this.todoView = todoView
     }
     fun onViewLoaded() {
         try {
-            todoView.showAllToDos(todoModel?.getAllToDoList())
+            todoView?.showAllToDos(todoModel?.getAllToDoList()!!)
         } catch (e:Exception) {
 
         }
@@ -22,8 +23,7 @@ class ToDoController {
 
     fun onAddButtonClicked(todo:ToDo) {
         try {
-            var isSuccess: Boolean = todoModel!!.addToDoItem(item)
-
+            var isSuccess: Boolean = todoModel!!.addToDoItem(todo)
         } catch(e:Exception) {
 
         }
