11bff11dd18753aac7e604f50825c87f3bc874f1
hieuvu99 <star.hieu.99@gmail.com>
2020-10-15 14:39:57 +0700

    refactor with gitignore

--- DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
+++ DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/DessertTimer.kt
@@ -17,6 +17,10 @@ package com.example.android.dessertclicker
 import android.os.Handler
 import timber.log.Timber
 import android.os.Looper
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
 /**
  * This is a class representing a timer that you can start or stop. The secondsCount outputs a count of
  * how many seconds since it started, every one second.
@@ -32,7 +36,7 @@ import android.os.Looper
  * https://developer.android.com/guide/components/processes-and-threads
  *
  */
-class DessertTimer {
+class DessertTimer(lifecycle: Lifecycle): LifecycleObserver {
 
     // The number of seconds counted since the timer started
     var secondsCount = 0
@@ -43,8 +47,11 @@ class DessertTimer {
      */
     private var handler = Handler(Looper.getMainLooper())
     private lateinit var runnable: Runnable
+    init {
+        lifecycle.addObserver(this)
+    }
 
-
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startTimer() {
         // Create the runnable action, which prints out a log and increments the seconds counter
         runnable = Runnable {
@@ -62,6 +69,7 @@ class DessertTimer {
         // Note that the Thread the handler runs on is determined by a class called Looper.
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopTimer() {
         // Removes all pending posts of runnable from the handler's queue, effectively stopping the
         // timer
--- DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.dessertclicker
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import android.view.ActionMode
 import android.view.Menu
@@ -29,6 +30,9 @@ import androidx.databinding.DataBindingUtil
 import com.example.android.dessertclicker.databinding.ActivityMainBinding
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+const val KEY_TIMER_SECONDS = "timer_seconds_key"
 class MainActivity : AppCompatActivity() {
 
     private var revenue = 0
@@ -74,8 +78,14 @@ class MainActivity : AppCompatActivity() {
             onDessertClicked()
         }
 
-        dessertTimer = DessertTimer()
-        dessertTimer.startTimer()
+        dessertTimer = DessertTimer(this.lifecycle)
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            dessertTimer.secondsCount =
+                    savedInstanceState.getInt(KEY_TIMER_SECONDS, 0)
+            showCurrentDessert()
+        }
         // Set the TextViews to the right values
         binding.revenue = revenue
         binding.amountSold = dessertsSold
@@ -101,8 +111,15 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStop() {
         super.onStop()
-        dessertTimer.stopTimer()
         Timber.i("onStop called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
+        outState.putInt(KEY_TIMER_SECONDS, dessertTimer.secondsCount)
+        super.onSaveInstanceState(outState)
+        Timber.i("onSaveInstanceState called")
     }
 
     override fun onDestroy() {
