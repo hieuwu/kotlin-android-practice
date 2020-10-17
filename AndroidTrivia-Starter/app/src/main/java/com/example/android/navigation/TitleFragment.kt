d0dbf0f10af163e7c88a62a7cc21ec4f2f74d300
hieuvu99 <star.hieu.99@gmail.com>
2020-10-17 10:02:26 +0700

    refactor: create set on click listener with navigation.create click listener

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -16,9 +16,10 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding:FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
-        binding.playButton.setOnClickListener {
-            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
-        }
+        binding.playButton.setOnClickListener (
+                Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+
         return binding.root
     }
 
