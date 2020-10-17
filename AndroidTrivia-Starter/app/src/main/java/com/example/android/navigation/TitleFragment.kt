a366f202a9862574d4acb7076ee73e332a810495
hieuvu99 <star.hieu.99@gmail.com>
2020-10-17 09:11:30 +0700

    refactor: add title fragement with data binding

---
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -0,0 +1,20 @@
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+class TitleFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val binding:FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
+        return binding.root
+    }
+
+}
