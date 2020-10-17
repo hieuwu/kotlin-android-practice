23ec3326a406c077e244348f9dbdcac2bb1e2d0d
hieuvu99 <star.hieu.99@gmail.com>
2020-10-17 09:58:10 +0700

    refactor:  create navigation on click with Navigation instead of view.findNavController

--- AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ AndroidTrivia-Starter/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import com.example.android.navigation.databinding.FragmentTitleBinding
 
@@ -15,8 +16,8 @@ class TitleFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val binding:FragmentTitleBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_title,container,false)
-        binding.playButton.setOnClickListener { view:View ->
-            view.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+        binding.playButton.setOnClickListener {
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
         }
         return binding.root
     }
