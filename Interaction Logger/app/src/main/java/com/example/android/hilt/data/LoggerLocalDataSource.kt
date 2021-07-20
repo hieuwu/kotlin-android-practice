fe9f2504e9a82ba98b321fa09640488ab5a63584
Hieu Vu <star.hieu.99@gmail.com>
2021-07-20 20:10:33 +0700

    Create save log with local database

---
+++ Interaction Logger/app/src/main/java/com/example/android/hilt/data/LoggerLocalDataSource.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (C) 2020 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.hilt.data
+
+import android.os.Handler
+import android.os.Looper
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import javax.inject.Inject
+import javax.inject.Singleton
+
+/**
+ * Data manager class that handles data manipulation between the database and the UI.
+ */
+@Singleton
+class LoggerLocalDataSource @Inject constructor(private val logDao: LogDao): LoggerDataSource {
+
+    private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
+    private val mainThreadHandler by lazy {
+        Handler(Looper.getMainLooper())
+    }
+
+    override
+    fun addLog(msg: String) {
+        executorService.execute {
+            logDao.insertAll(
+                Log(
+                    msg,
+                    System.currentTimeMillis()
+                )
+            )
+        }
+    }
+
+    override
+    fun getAllLogs(callback: (List<Log>) -> Unit) {
+        executorService.execute {
+            val logs = logDao.getAll()
+            mainThreadHandler.post { callback(logs) }
+        }
+    }
+    override
+    fun removeLogs() {
+        executorService.execute {
+            logDao.nukeTable()
+        }
+    }
+}
