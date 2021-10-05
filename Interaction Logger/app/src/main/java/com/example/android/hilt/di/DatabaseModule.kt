d4d3ce27b54fb16e14b456abb8408be046637d23
Hieu Vu <star.hieu.99@gmail.com>
2021-10-05 20:03:07 +0700

    Create project for DI

---
+++ Interaction Logger/app/src/main/java/com/example/android/hilt/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.example.android.hilt.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.android.hilt.data.AppDatabase
+import com.example.android.hilt.data.LogDao
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): AppDatabase {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabase::class.java,
+            "logging.db"
+        ).build()
+    }
+
+    @Provides
+    fun provideLogDao(database: AppDatabase): LogDao {
+        return database.logDao()
+    }
+}
