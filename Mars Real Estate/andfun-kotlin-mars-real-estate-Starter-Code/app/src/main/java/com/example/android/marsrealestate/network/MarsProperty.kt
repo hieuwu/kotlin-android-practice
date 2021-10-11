b1cc4fc194c07c685eec3c462895c927572cd3d9
Hieu Vu <star.hieu.99@gmail.com>
2021-10-11 20:59:35 +0700

    Create mar estate to practice retrofit

---
+++ Mars Real Estate/andfun-kotlin-mars-real-estate-Starter-Code/app/src/main/java/com/example/android/marsrealestate/network/MarsProperty.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.example.android.marsrealestate.network
+
+import android.os.Parcelable
+import com.squareup.moshi.Json
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MarsProperty(
+    val id: String,
+    val type: String,
+    @Json(name = "img_src")
+    val imgSrcUrl: String,
+    val price: Double
+) : Parcelable {
+    val isRental
+        get() = type == "rent"
+}
